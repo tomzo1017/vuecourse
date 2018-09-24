9a814aa2145f0ef0c1dd33bd782a233d1b381000
Tommi <tommihautaviita@gmail.com>
2018-09-25 00:19:55 +0300

    home

--- vue-app/resources/views/welcome.blade.php
+++ vue-app/resources/views/welcome.blade.php
@@ -15,11 +15,11 @@
     <body>
        <div id="app">
         <ul>
-            <li v-for="skill in skills"> @{{ skill }}</li>
+            <li v-for="skill in skills" v-text="skill"> </li>
         </ul>
        </div>
        <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
        <script src="https://cdn.jsdelivr.net/npm/vue@2.5.17/dist/vue.js"></script>
-        <script src="./js/app.js/"></script>
+        <script src="./js/app.js"></script>
     </body>
 </html>
--- vue-app/routes/web.php
+++ vue-app/routes/web.php
@@ -15,6 +15,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/skills', function() {
+Route::get('skills', function() {
     return ['Laravel', 'Vue', 'PHP', 'Javascript', 'Tooling'];
 });
